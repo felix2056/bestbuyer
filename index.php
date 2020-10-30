33f4ca0acffe808f25fb60950479b9e70dd26a47
felix2056 <felixdaniel2056@gmail.com>
2020-10-30 22:50:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php include_once('includes/header.php'); ?>
+
+<?php include_once('includes/nav.php'); ?>
+
+<div class="page-container drawer-page-content" id="PageContainer">
+
+    <main class="main-content js-focus-hidden" id="MainContent" role="main" tabindex="-1">
+      <!-- BEGIN content_for_index -->
+      <div id="shopify-section-hero-1" class="shopify-section index-section index-section--flush">
+        <div data-section-id="hero-1" data-section-type="hero-section">
+          <div class="hero hero--large hero-hero-1 hero__overlay box ratio-container js lazyloaded" id="Hero-hero-1"
+            data-layout="full_width"
+            data-bg="//cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_300x300.jpg?v=1603798068"
+            data-bgset="//cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_180x.jpg?v=1603798068 180w 101h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_360x.jpg?v=1603798068 360w 203h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_540x.jpg?v=1603798068 540w 304h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_720x.jpg?v=1603798068 720w 405h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_900x.jpg?v=1603798068 900w 506h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1080x.jpg?v=1603798068 1080w 608h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1296x.jpg?v=1603798068 1296w 729h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1512x.jpg?v=1603798068 1512w 851h,
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1728x.jpg?v=1603798068 1728w 972h,
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping.jpg?v=1603798068 1920w 1080h"
+            data-parent-fit="cover"
+            style="background-position: center center; background-image: url(&quot;https://cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1512x.jpg?v=1603798068&quot;);"
+            alt="" src="//cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_300x300.jpg?v=1603798068">
+            <div class="hero__inner">
+              <div class="page-width text-center">
+                <h2 class="h1 mega-title mega-title--large">50% OFF</h2>
+                <div class="rte-setting mega-subtitle mega-subtitle--large">
+                  <p>Limited Items Only.</p>
+                </div>
+              </div>
+            </div>
+            <picture style="display: none;">
+              <source
+                data-srcset="//cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_180x.jpg?v=1603798068 180w 101h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_360x.jpg?v=1603798068 360w 203h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_540x.jpg?v=1603798068 540w 304h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_720x.jpg?v=1603798068 720w 405h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_900x.jpg?v=1603798068 900w 506h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1080x.jpg?v=1603798068 1080w 608h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1296x.jpg?v=1603798068 1296w 729h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1512x.jpg?v=1603798068 1512w 851h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1728x.jpg?v=1603798068 1728w 972h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping.jpg?v=1603798068 1920w 1080h"
+                sizes="1349px"
+                srcset="//cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_180x.jpg?v=1603798068 180w 101h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_360x.jpg?v=1603798068 360w 203h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_540x.jpg?v=1603798068 540w 304h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_720x.jpg?v=1603798068 720w 405h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_900x.jpg?v=1603798068 900w 506h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1080x.jpg?v=1603798068 1080w 608h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1296x.jpg?v=1603798068 1296w 729h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1512x.jpg?v=1603798068 1512w 851h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_1728x.jpg?v=1603798068 1728w 972h, //cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping.jpg?v=1603798068 1920w 1080h">
+              <img alt="" class="lazyautosizes lazyloaded" data-sizes="auto" data-parent-fit="cover" sizes="1349px">
+            </picture>
+          </div>
+          <noscript>
+            <div class="hero hero--large hero__overlay"
+              style="background-image: url('//cdn.shopify.com/s/files/1/0504/4718/8121/files/vintage-shopping_2048x.jpg?v=1603798068'); background-position: center center;">
+            </div>
+          </noscript>
+        </div>
+
+
+      </div><!-- END content_for_index -->
+
+    </main>
+
+    <?php include_once('includes/footer.php'); ?>
+
+    <div id="slideshow-info" class="visually-hidden" aria-hidden="true">Use left/right arrows to navigate the slideshow
+      or swipe left/right if using a mobile device</div>
+
+  </div>
+
+  <?php include_once('includes/modals.php'); ?>
+</body>
